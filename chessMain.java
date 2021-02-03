98a2529981c6b27a36daf0cebb603f93d15c38a9
William Stobaugh <william.jamesstobaugh@gmail.com>
2021-02-02 18:27:59 -0800

    Pop

--- chessMain.java
+++ chessMain.java
@@ -1,7 +1,23 @@
 public class chessMain
 {
+    String[][] chessBoard = new String[7][7];
+    chessBoard[0][0] = "yo";
+
     public void main()
     {
-        
+        // start the game
+
+            // initialize chess pieces
+            pieceInit();
     }
+
+
+
+    public static void pieceInit()
+    {
+        // give each side
+
+    }
+
+
 }
