5caead25f72e79ca43f9bd98bfc1d2c67e706bbd
Sophie Noureddine <sophienoureddine6@gmail.com>
2021-02-11 18:23:02 -0800

    Queen subclass

---
+++ Queen.java
@@ -0,0 +1,7 @@
+private class Queen extends Piece {
+    private boolean isWhite;
+    private int x;
+    private int y;
+    public Queen(boolean color, int number)
+    super(int x, int y, boolean isWhite)
+}
