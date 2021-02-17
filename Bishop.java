dbfb5f98f2edbb94b8ba9d79b015df6665a91f82
Anika Nath <anikanath1@gmail.com>
2021-02-16 21:42:34 -0800

    Bishop subclass

---
+++ Bishop.java
@@ -0,0 +1,8 @@
+public class Bishop extends Piece {
+
+    public Bishop(int x, int y, boolean isWhite)
+    {
+        super(x, y, isWhite);
+        this.x = x;
+        this.y = y;
+        this.isWhite = isWhite;
