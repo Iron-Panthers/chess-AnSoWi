34f3e301bca4c02310606d14d6653c8629929872
William Stobaugh <william.jamesstobaugh@gmail.com>
2021-02-16 17:50:22 -0800

    Fixed Queen and Rook files
    Whoever wrote the rook file Im sorry but I commented out a lot of your stuff, I'll see if we can use any of it later.

--- Queen.java
+++ Queen.java
@@ -1,7 +1,15 @@
 public class Queen extends Piece {
-    private boolean isWhite;
-    private int x;
-    private int y;
-    public Queen(boolean color, int number)
-    super(int x, int y, boolean isWhite)
+    // private boolean isWhite;
+    // private int x;
+    // private int y;
+
+    public Queen(int x, int y, boolean isWhite)
+    {
+        super(x, y, isWhite);
+        this.x = x;
+        this.y = y;
+        this.isWhite = isWhite;
+
+    }
+    
 }
