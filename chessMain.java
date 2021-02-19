910c29a0aa38e3002df20e26283e5756f6ff38cc
William Stobaugh <william.jamesstobaugh@gmail.com>
2021-02-18 18:30:08 -0800

    Starting to place pieces on board

--- chessMain.java
+++ chessMain.java
@@ -1,30 +1,64 @@
 public class chessMain
 {
-    public static Piece[][] chessBoard = new Piece[8][8];
+
+    // Variable names fit their uses
+    public static Piece[][] chessBoard, whitePieces, blackPieces = new Piece[8][8];
     public static void main(String[] args)
     {
         // initialize chess pieces
         pieceInit();
         printBoard();
-        Rook one = new Rook(8, 8, true);
+        
     }
 
 
 
     public static void pieceInit()
     {
-        // fill board with empty spaces
+        // fill piece arrays with null spaces (clear out anything if there)
         for (int y = 0; y <= 7; y++)
         {
             for (int x = 0; x <= 7; x++)
             {
                     chessBoard[x][y] = null;
+                    whitePieces[x][y] = null;
+                    blackPieces[x][y] = null;
             }
             
         }
 
+            // assign each piece for piece array
+        /*
+        ATTENTION, FOR WHITE/BLACK PIECES, X = TYPE, Y = INDEX
 
-        // fill in all of the pieces.
+        X~~
+        0: PAWNS
+        1: ROOKS
+        2: BISHOPS
+        3: KNIGHTS
+        4: QUEEN
+        5: KING
+        
+        */
+
+        // assign Pawns
+        for (int i = 0; i < 7; i++)
+        {
+            whitePieces[0][i] = new Pawn(i, 6, true);
+            blackPieces[0][i] = new Pawn(i, 1, false);
+
+            chessBoard[i][0] = blackPieces[0][i];
+            chessBoard[i][6] = whitePieces[0][i];
+        }
+
+        // assign Rooks
+        whitePieces[1][0] = new Rook(0, 7, true); whitePieces[1][1] = new Rook(7, 7, true);
+        blackPieces[1][0] = new Rook(0, 0, false); whitePieces[1][1] = new Rook(7, 0, false);
+
+        // assign   
+
+
+        
 
     }
 
@@ -47,6 +81,8 @@ public class chessMain
              System.out.println("");
          }
     }
+
+    
 
 
 }
