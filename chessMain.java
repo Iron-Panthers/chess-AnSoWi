d32dee2c089b9f7e028ec7feccc967e96c9691e8
Jake R <jake.b.rothstein@gmail.com>
2021-03-02 18:14:28 -0800

    Changed chess piece symbols
    from eg. WP-1 to ♙(W)

--- chessMain.java
+++ chessMain.java
@@ -47,16 +47,16 @@ public class chessMain
         // assign Pawns
         for (int i = 0; i < 8; i++)
         {
-            whitePieces[0][i] = new Pawn(i, 6, true, "WP - " + (i + 1));
-            blackPieces[0][i] = new Pawn(i, 1, false, "BP - " + (i + 1));
+            whitePieces[0][i] = new Pawn(i, 6, true, "♙(W)" + (i + 1));
+            blackPieces[0][i] = new Pawn(i, 1, false, "♟︎(B)" + (i + 1));
 
             chessBoard[i][1] = blackPieces[0][i];
             chessBoard[i][6] = whitePieces[0][i];
         }
 
         // assign Rooks
-        whitePieces[1][0] = new Rook(0, 7, true, "WR - 1"); whitePieces[1][1] = new Rook(7, 7, true, "WR - 2");
-        blackPieces[1][0] = new Rook(0, 0, false, "BR - 1"); blackPieces[1][1] = new Rook(7, 0, false, "BR - 2");
+        whitePieces[1][0] = new Rook(0, 7, true, "♖(W)"); whitePieces[1][1] = new Rook(7, 7, true, "WR - 2");
+        blackPieces[1][0] = new Rook(0, 0, false, "♜(B)"); blackPieces[1][1] = new Rook(7, 0, false, "BR - 2");
 
         // assign rooks to board
 
@@ -65,24 +65,24 @@ public class chessMain
 
 
         // Assign knights
-        whitePieces[3][0] = new Knight(1, 7, true, "WK - 1"); whitePieces[3][1] = new Knight(6, 7, true, "WK - 2");
-        blackPieces[3][0] = new Knight(1, 0, false, "BK - 1"); blackPieces[3][1] = new Knight(6, 0, false, "BK - 2");
+        whitePieces[3][0] = new Knight(1, 7, true, "♘(W)"); whitePieces[3][1] = new Knight(6, 7, true, "WK - 2");
+        blackPieces[3][0] = new Knight(1, 0, false, "♞(B)"); blackPieces[3][1] = new Knight(6, 0, false, "BK - 2");
 
         //Assign knights to board
         chessBoard[1][0] = blackPieces[3][0];   chessBoard[6][0] = blackPieces[3][1];
         chessBoard[1][7] = whitePieces[3][0];   chessBoard[6][7] = whitePieces[3][1];
 
         //Assign Bishops
-        whitePieces[2][0] = new Bishop(2, 7, true, "WB - 1"); whitePieces[2][1] = new Bishop(5, 7, true, "WB - 2");
-        blackPieces[2][0] = new Bishop(2, 0, false, "BB - 1"); blackPieces[2][1] = new Bishop(5, 0, false, "BB - 2");
+        whitePieces[2][0] = new Bishop(2, 7, true, "♗(W)"); whitePieces[2][1] = new Bishop(5, 7, true, "WB - 2");
+        blackPieces[2][0] = new Bishop(2, 0, false, "♝(B)"); blackPieces[2][1] = new Bishop(5, 0, false, "BB - 2");
         
         //Assign Bishops to board
         chessBoard[2][0] = blackPieces[2][0];   chessBoard[5][0] = blackPieces[2][1];
         chessBoard[2][7] = whitePieces[2][0];   chessBoard[5][7] = whitePieces[2][1];
 
         // assign queen/king
-        whitePieces[4][0] = new Queen(3, 7, true, "WQ - 1"); whitePieces[5][0] = new King(4, 7, true, "WK* - 1");
-        blackPieces[4][0] = new Queen(3, 0, false, "BQ - 1"); blackPieces[5][0] = new King(4, 0, false, "BK* - 1");
+        whitePieces[4][0] = new Queen(3, 7, true, "♕(W)"); whitePieces[5][0] = new King(4, 7, true, "WK* - 1");
+        blackPieces[4][0] = new Queen(3, 0, false, "♛(B)"); blackPieces[5][0] = new King(4, 0, false, "BK* - 1");
 
         //Assign king/queen to board
         chessBoard[3][0] = blackPieces[4][0];   chessBoard[4][0] = blackPieces[5][0];
